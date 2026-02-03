bd9664b89470db0906a866128882f3cac638449f
Azhar Sayyad <sayyadazhar04@gmail.com>
2026-02-03 14:15:00 +0530

    Added tests for application bootstrap

---
+++ backend/src/test/java/com/atsdoctor/backend/AtsDoctorApplicationTests.java
@@ -0,0 +1,12 @@
+package com.atsdoctor.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AtsDoctorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+}
